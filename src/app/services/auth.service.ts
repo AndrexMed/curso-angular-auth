1e21cffd1b8490140917d6d072488bb81a018507
GiovanniAlzate <galzate@cesde.edu.co>
2023-10-23 09:35:52 -0500

    Flujo de registro

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,20 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(email: string, password: string) {
-    return this.http.post(`${this.apiUrl}/api/v1/auth/login`, {email, password})
+    return this.http.post(`${this.apiUrl}/api/v1/auth/login`, { email, password })
+  }
+
+  register(name: string, email: string, password: string) {
+    return this.http
+      .post(`${this.apiUrl}/api/v1/auth/register`, { name, email, password })
+      .pipe(
+        catchError((error) => {
+          //console.log("errror from service ", error)
+          if (error.error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
+            return throwError('The user already exist');
+          }
+          return throwError('Ups algo salio mal');
+        })
+      )
   }
 }
