6276f814c95bd19a92964493cec4876ead06214b
GiovanniAlzate <galzate@cesde.edu.co>
2023-10-23 15:09:13 -0500

    Implementacion del password recovery

--- src/app/modules/auth/components/forgot-password-form/forgot-password-form.component.ts
+++ src/app/modules/auth/components/forgot-password-form/forgot-password-form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { RequestStatus } from '@models/request.status.model';
+import { AuthService } from '@services/auth.service';
 @Component({
   selector: 'app-forgot-password-form',
   templateUrl: './forgot-password-form.component.html'
@@ -9,18 +11,31 @@ export class ForgotPasswordFormComponent {
   form = this.formBuilder.nonNullable.group({
     email: ['', [Validators.email, Validators.required]],
   });
-  status: string = 'init';
+
+  status: RequestStatus = 'init';
   emailSent = false;
 
   constructor(
     private formBuilder: FormBuilder,
+    private authSvc: AuthService
   ) { }
 
   sendLink() {
     if (this.form.valid) {
       this.status = 'loading';
       const { email } = this.form.getRawValue();
-      // TODO: Connect
+
+      this.authSvc.recovery(email)
+        .subscribe({
+          next: () => {
+            this.status = 'success'
+            this.emailSent = true
+          },
+          error: (error) => {
+            this.status = 'failed'
+            console.log(error)
+          }
+        })
     } else {
       this.form.markAllAsTouched();
     }
--- src/app/modules/auth/components/recovery-form/recovery-form.component.ts
+++ src/app/modules/auth/components/recovery-form/recovery-form.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { RequestStatus } from '@models/request.status.model';
+import { AuthService } from '@services/auth.service';
 
 import { CustomValidators } from '@utils/validators';
 
@@ -9,9 +12,10 @@ import { CustomValidators } from '@utils/validators';
   templateUrl: './recovery-form.component.html',
 })
 export class RecoveryFormComponent {
+
   form = this.formBuilder.nonNullable.group(
     {
-      newPassword: ['', [Validators.minLength(6), Validators.required]],
+      newPassword: ['', [Validators.minLength(8), Validators.required]],
       confirmPassword: ['', [Validators.required]],
     },
     {
@@ -20,16 +24,45 @@ export class RecoveryFormComponent {
       ],
     }
   );
-  status: string = 'init';
+
+  status: RequestStatus = 'init';
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
 
-  constructor(private formBuilder: FormBuilder) {}
+  token = ''
+
+  constructor(private formBuilder: FormBuilder,
+    private authSvc: AuthService,
+    private route: ActivatedRoute,
+    private router: Router) {
+    this.route.queryParamMap.subscribe(params => {
+      const token = params.get('token')
+
+      if (token) {
+        this.token = token
+      } else {
+        this.router.navigate(['/login'])
+      }
+    })
+  }
 
   recovery() {
     if (this.form.valid) {
-      // Todo
+      const { newPassword } = this.form.getRawValue()
+
+      this.status = 'loading'
+
+      this.authSvc.changePassword(this.token, newPassword)
+        .subscribe({
+          next: () => {
+            this.status = 'success'
+            this.router.navigate(['/login'])
+          },
+          error: () => {
+            this.status = 'failed'
+          }
+        })
     } else {
       this.form.markAllAsTouched();
     }
