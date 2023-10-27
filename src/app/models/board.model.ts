9f3db49fae0dc48b06ce6f81d07f697688a6cecb
GiovanniAlzate <galzate@cesde.edu.co>
2023-10-27 16:56:05 -0500

    Detalle de un board

--- src/app/models/board.model.ts
+++ src/app/models/board.model.ts
@@ -1,9 +1,13 @@
 import { User } from "./users.model";
 import { Colors } from "./color.model";
+import { List } from "./list.model";
+import { Card } from "./card.model";
 
 export interface Board{
     id: string;
     title: string;
     backgroundColor: Colors;
     members: User[];
+    lists: List[]
+    cards: Card[]
 }
